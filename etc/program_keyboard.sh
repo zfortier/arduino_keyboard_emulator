736a48cfe2f9b7daa6722dc6f4c1faf5798ca0b1
zfortier <zfortier@gmail.com>
2020-05-13 13:04:56 -0500

    Update program_keyboard.sh

--- etc/program_keyboard.sh
+++ etc/program_keyboard.sh
@@ -1,64 +1,53 @@
 #!/bin/bash
 
-echo "This script will program an Arduino to act as a USB keyboard. The Arduino needs to be plugged in via USB and"
-echo "the UART needs to be disconnected. Optionally, an LED can be connected to Digital Pin 2."
-echo "Set up your hardware and press enter to continue..."
-read
+function check_rv() {
+  rv=$1
+  shift
+  expected_rv=$1
+  shift
+  if [ "${rv}" -ne "${expected_rv}" ]; then
+    printf "$*"
+    exit 1
+  fi
+}    
 
+printf "This script will program an Arduino to act as a USB keyboard.\n" \
+       " The Arduino needs to be plugged in via USB and the UART needs to\n" \
+       " be disconnected. Optionally, an LED can be connected to Digital\n" \
+       " Pin 2. Set up your hardware and press enter to continue..."
+read
 echo "Uploading keyboard program to device..."
 make clean
-if [ $? != 0 ]; then
-  echo "error with gmake. Check the source code and Makefile."
-  exit -1
-fi
-
+check_rv $? 0 "error with gmake. Check the source code and Makefile.\n"
 make
-if [ $? != 0 ]; then
-  echo "error with gmake. Check the source code and Makefile."
-  exit -1
-fi
-
+check_rv $? 0 "error with gmake. Check the source code and Makefile.\n"
 make upload
-if [ $? != 0 ]; then
-  echo "error with gmake or avrdude. Check the source code and Makefile. Confirm  that the correct"
-  echo "board model is specified in the Makefile. Make sure avr-gcc/avr-g++/avrdude are installed."
-  exit -1
-fi
+check_rv $? 0 "error with gmake or avrdude. Check the source code and\n" \
+              " Makefile. Confirm  that the correct\nboard model is\n" \
+              " set in Makefile. Install avr-gcc/avr-g++/avrdude.\n"
 sleep 2
-
-echo "Successfully uploaded the keyboard scan code program. Proceeding to USB reset..."
+printf "Successfully uploaded the keyboard program. Proceeding to USB reset...\n"
 sleep 2
-
-echo "The USB chip needs to be flashed with the Keyboard frimware."
-echo "To do that, you need to jump the USB reset pins on the device. They may not have headers soldered. Locate and"
-echo "jump those pads. The LED's might flash, but it's not guaranteed"
-echo "Press enter to continue..."
+printf "The USB chip needs to be flashed with the Keyboard frimware. To do\n" \
+       " that, you need to jump the USB reset pins on the device. They may not\n" \
+       " have headers soldered. Locate and jump those pads. The LED's might \n" \
+       " flash, but it's not guaranteed\n\nPress enter to continue...\n."
 read
-
 dfu-programmer atmega16u2 erase
-if [ $? != 5 ]; then
-  echo "error erasing USB controller. Try jumping the reset pins again"
-  exit -1
-fi
+check_rv $? 5 "error erasing USB controller. Try jumping the reset pins again"
 sleep 2
-
-dfu-programmer atmega16u2 flash firmware/kbd.hex 
-if [ $? != 0 ]; then
-  echo "error flashing the keyboard firmware to device. Try re-running this script."
-  exit -1
-fi
+dfu-programmer atmega16u2 flash firmware/kbd.hex
+check_rv $? 0 "error flashing the keyboard firmware to device.\n" \
+              " Try re-running this script.\n"
 sleep 2
-
 dfu-programmer atmega16u2 reset
-if [ $? != 0 ]; then
-  echo "error resetting the device. It may have been properly flashed anyway, unplug it for 10 seconds then test."
-  exit -1
-fi
+check_rv $? 0 "error resetting the device. It may have been properly flashed\n" \
+              " anyway, unplug it for 10 seconds then test.\n"
 sleep 2
-
-echo "Successfully uploaded the keyboard scan code emulator and flashed the USB controller as a keyboard!"
-echo "Now you need to unplug the deveice from USB and power (it needs to be off) for 5-10 seconds."
-echo "Afterwards, plug it in and test the keyboard functionality by connecting the UART bridge to pins 7 & 8"
-echo "and connecting to the device with a terminal program at a baud rate of 9600."
+printf "Successfully uploaded the keyboard scan code emulator and flashed the\n" \
+        " USB controller as a keyboard! Now you need to unplug the deveice from\n" \
+        " USB and power (it needs to be off) for 5-10 seconds. Afterwards, plug\n" \
+        " it in and test the keyboard functionality by connecting the UART bridge\n" \
+        " to pins 7 & 8 and connecting to the device with a terminal program at a\n" \
+        "baud rate of 9600.\n\n"
 exit 0
-
